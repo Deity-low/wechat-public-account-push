0f003b773384f2f60ac66629d069d8d24a4091df
Deity <56100234+Deity-low@users.noreply.github.com>
2023-05-26 16:51:54 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oQ_QT51SjCDnZz021AXmNO9vh-II',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'wCnaRKAKHwNEfM1Ji2oohZzXDOQrIAIOBASFi_CbiZ4',
+      useTemplateId: 'E6E3HSiyXEVdpxQrkp0PjMY4Or5jFjmtVzldd5v7irI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-30',
       festivals: [
